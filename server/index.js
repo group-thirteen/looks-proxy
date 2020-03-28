c421241d69315ee35da23924278f4d784f81bf9d
Vincent Huang <vincent.kim.huang@gmail.com>
2020-03-28 12:04:18 -0700

    Prepare proxy server for deployment

--- server/index.js
+++ server/index.js
@@ -12,8 +12,8 @@ app.listen(port, () => {
 
 app.use(express.static(path.join(__dirname, '../public')));
 
-const imagesTarget = 'http://localhost:3000';
-const lookTarget = 'http://localhost:3001';
+const imagesTarget = 'http://ec2-3-101-33-181.us-west-1.compute.amazonaws.com/';
+const lookTarget = 'http://ec2-18-144-75-47.us-west-1.compute.amazonaws.com/';
 const reviewTarget = 'http://localhost:8080';
 
 app.all('/images/:id', (req, res) => {
@@ -29,4 +29,9 @@ app.all('/api/getimageurls', (req, res) => {
 app.all('/reviews', (req, res) => {
   console.log('redirecting to server 3');
   apiProxy.web(req, res, { target: reviewTarget });
+});
+
+app.all('/api/updateLike', (req, res) => {
+  console.log('redirecting to server2');
+  apiProxy.web(req, res, { target: lookTarget });
 });
